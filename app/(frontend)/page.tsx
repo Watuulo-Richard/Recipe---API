6ac7da40832e5561fb81195e36c0d318916a9444
Watuulo-Richard <watuulorichard@gmail.com>
2025-03-14 11:10:16 +0300

    feat: initial commit

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -7,7 +7,11 @@ export type CurrencyItemType = {
   rate: number;
 };
 export default async function page() {
-  const fetchedProducts = await fetch('/api/v1/recipeproducts').then(res => res.json());
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+
+const API = `${baseUrl}/api/v1/recipeproducts`
+
+  const fetchedProducts = await fetch(API).then(res => res.json());
   const products = fetchedProducts?.data
   const fetchedCategories = await fetchCategories()
 
